a2a0feb2d3f689debc971f1f14a970b5e38ef0b5
eyyytran <ltatran94@gmail.com>
2022-07-09 19:37:53 -0400

    added spotify client authentication

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const chillStudyMusic = '7MMrQJA2EgIgjanx9acsAZ'
-
-const playbutton = document.querySelector('#togglePlay')
-const container = document.querySelector('#container')
-
 const clientId = config.clientId
 const clientSecret = config.clientSecret
+
+// https://accounts.spotify.com/authorize?client_id=shfjkdshfkjshkfhsd&redirect_uri=http:fsdfdsjkfhskjfhs.com%2Fcallback&scope=user-read-private&20user-read-email&response_type=token&state=123
+
+const embedIframe = document.querySelector('#embed-iframe')
+const button = document.querySelector('.log-in')
+
+const SPOTIFY_AUTHORIZE_ENDPOINT = 'https://accounts.spotify.com/authorize'
+const REDIRECT_URI_AFTER_LOGIN =
+    'http://127.0.0.1:5500/DC-Front-End-Project/index.html/callback'
+const SCOPES = ['user-read-currently-playing', 'user-read-playback-state']
+const SPACE_DELIMITER = '%20'
+const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER)
+
+const handleLogin = () => {
+    window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${clientId}&redirect_uri=${REDIRECT_URI_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`
+}
+
+button.addEventListener('click', () => {
+    handleLogin()
+})
 
 const getToken = async () => {
     const result = await fetch('https://accounts.spotify.com/api/token', {
@@ -19,10 +34,10 @@ const getToken = async () => {
     return data.access_token
 }
 
-const getTrack = async track_id => {
+const getTrack = async () => {
     const token = await getToken()
     const result = await fetch(
-        `https://api.spotify.com/v1/tracks/${track_id}`,
+        `https://api.spotify.com/v1/users/22miwnisbrauyz66lvgaxjnqa/playlists`,
         {
             method: 'GET',
             headers: {
@@ -31,5 +46,23 @@ const getTrack = async track_id => {
         }
     )
     const data = await result.json()
-    console.log(data.uri)
+    console.log(data)
+    console.log('easy playlist', data.items[2].id)
+    console.log('medium playlist', data.items[0].id)
+    console.log('hard playlist', data.items[1].id)
+    const easyMusic = data.items[2].id
+    //make the iframe
+    const makeIframe = document.createElement('iframe')
+    makeIframe.id = 'spotify-player'
+    makeIframe.style = 'border-radius:12px'
+    makeIframe.src = `https://open.spotify.com/embed/playlist/${easyMusic}?utm_source=generator`
+    makeIframe.width = '100%'
+    makeIframe.height = '100%'
+    makeIframe.frameBorder = '0'
+    makeIframe.allowfullscreen = ''
+    makeIframe.allow =
+        'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
+    embedIframe.append(makeIframe)
 }
+
+getTrack()
