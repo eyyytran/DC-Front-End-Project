7eb0be60ade42c4338e76307b4c39f3b60f99601
eyyytran <ltatran94@gmail.com>
2022-07-09 20:16:54 -0400

    starting creation of front page

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const clientId = config.clientId
 const clientSecret = config.clientSecret
 
-// https://accounts.spotify.com/authorize?client_id=shfjkdshfkjshkfhsd&redirect_uri=http:fsdfdsjkfhskjfhs.com%2Fcallback&scope=user-read-private&20user-read-email&response_type=token&state=123
-
 const embedIframe = document.querySelector('#embed-iframe')
 const button = document.querySelector('.log-in')
 
+//Enable the user to log into their spotify
 const SPOTIFY_AUTHORIZE_ENDPOINT = 'https://accounts.spotify.com/authorize'
 const REDIRECT_URI_AFTER_LOGIN =
-    'http://127.0.0.1:5500/DC-Front-End-Project/index.html/callback'
+    'http://127.0.0.1:5500/DC-Front-End-Project/index.html'
 const SCOPES = ['user-read-currently-playing', 'user-read-playback-state']
 const SPACE_DELIMITER = '%20'
 const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER)
@@ -16,10 +15,6 @@ const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER)
 const handleLogin = () => {
     window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${clientId}&redirect_uri=${REDIRECT_URI_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`
 }
-
-button.addEventListener('click', () => {
-    handleLogin()
-})
 
 const getToken = async () => {
     const result = await fetch('https://accounts.spotify.com/api/token', {
@@ -55,9 +50,10 @@ const getTrack = async () => {
     const makeIframe = document.createElement('iframe')
     makeIframe.id = 'spotify-player'
     makeIframe.style = 'border-radius:12px'
-    makeIframe.src = `https://open.spotify.com/embed/playlist/${easyMusic}?utm_source=generator`
+
+    makeIframe.src = `https://open.spotify.com/embed/playlist/${easyMusic}?utm_source=generator&theme=0`
     makeIframe.width = '100%'
-    makeIframe.height = '100%'
+    makeIframe.height = '130px' //this will need to become 80px in web mode
     makeIframe.frameBorder = '0'
     makeIframe.allowfullscreen = ''
     makeIframe.allow =
@@ -66,3 +62,8 @@ const getTrack = async () => {
 }
 
 getTrack()
+
+button.addEventListener('click', () => {
+    handleLogin()
+    button.display = 'none'
+})
