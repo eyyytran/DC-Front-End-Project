af61d44560888ab3b36b84be80d5dc09ab82791e
eyyytran <ltatran94@gmail.com>
2022-07-11 14:19:59 -0400

    added navigation

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const clientId = config.clientId
 const clientSecret = config.clientSecret
 
+const playListMenu = document.querySelector('.playlist-menu')
+
 const embedIframe = document.querySelector('#embed-iframe')
 const playEasy = document.querySelector('#easy')
 const playMedium = document.querySelector('#medium')
 const playHard = document.querySelector('#hard')
 const greeting = document.querySelector('.greeting')
 
+const musicBtn = document.querySelector('#music-button')
+const timerBtn = document.querySelector('#timer-button')
+const userBtn = document.querySelector('#user-button')
+
+///music player functions
 const getToken = async () => {
     const result = await fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
@@ -44,10 +51,10 @@ const getTrack = async userChoice => {
 
     const makeIframe = document.createElement('iframe')
     makeIframe.id = 'spotify-player'
-    makeIframe.style = 'border-radius:0px'
+    makeIframe.style = 'border-radius:12px'
     makeIframe.src = `https://open.spotify.com/embed/playlist/${playlist}?utm_source=generator&theme=0`
     makeIframe.width = '100%'
-    makeIframe.height = '130px' //this will need to become 80px in web mode
+    makeIframe.height = '120px' //this will need to become 80px in web mode
     makeIframe.frameBorder = '0'
     makeIframe.allowfullscreen = ''
     makeIframe.allow =
@@ -58,16 +65,23 @@ const getTrack = async userChoice => {
 playEasy.addEventListener('click', () => {
     embedIframe.innerHTML = null
     getTrack('easy')
-    greeting.style.display = 'none'
 })
 
 playMedium.addEventListener('click', () => {
     embedIframe.innerHTML = null
     getTrack('medium')
-    greeting.style.display = 'none'
 })
 playHard.addEventListener('click', () => {
     embedIframe.innerHTML = null
     getTrack('hard')
-    greeting.style.display = 'none'
+})
+
+//Nav Bar Functions
+
+musicBtn.addEventListener('click', () => {
+    playListMenu.style.display = 'grid'
+})
+
+timerBtn.addEventListener('click', () => {
+    playListMenu.style.display = 'none'
 })
